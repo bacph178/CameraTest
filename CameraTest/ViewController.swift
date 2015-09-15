eee14d8dba8859376433cda96fa5fb347f6bf079
Phùng Hoàng Bắc <phunghoangbac@Phungs-Mac-mini.local>
2015-09-15 16:46:11 +0700

    Initial Commit

---
+++ CameraTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraTest
+//
+//  Created by Phùng Hoàng Bắc on 9/15/15.
+//  Copyright (c) 2015 Phùng Hoàng Bắc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
